4e3f703b2911fac62a433e51ff122b59688b6e7c
Alberto Morgante <alberto.morgante@orangex.es>
2018-11-27 20:47:31 +0100

    adding terms and policy

--- handler.go
+++ handler.go
@@ -38,6 +38,7 @@ func (h *MiBebe) OnLaunch(context context.Context, request *alexa.Request, sessi
 
 	response.SetStandardCard(cfg.CardTitle, cfg.SpeechOnLaunch, cfg.ImageSmall, cfg.ImageLong)
 	response.SetOutputText(cfg.SpeechOnLaunch)
+	response.ShouldSessionEnd = false
 	return nil
 }
 
