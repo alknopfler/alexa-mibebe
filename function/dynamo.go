4822fc1bdb6cd8d78d644055d67f399c058c477f
Alberto Morgante <alberto.morgante@orangex.es>
2018-12-20 11:34:38 +0100

    add the new query to get just last value

--- function/dynamo.go
+++ function/dynamo.go
@@ -131,15 +131,17 @@ func getRecordsPeso(key, value, oldTime, newTime string) ([]RecordPeso, error){
 		log.Println("Got error building expression: "+err.Error())
 		return nil, err
 	}
-	params := &dynamodb.ScanInput{
+	params := &dynamodb.QueryInput{
 		ExpressionAttributeNames:  expr.Names(),
 		ExpressionAttributeValues: expr.Values(),
 		FilterExpression:          expr.Filter(),
 		ProjectionExpression:      expr.Projection(),
+		ScanIndexForward: 		   aws.Bool(false),
+		Limit: 					   aws.Int64(1),
 		TableName:                 aws.String(cfg.DynamoTablePeso),
 	}
 
-	result, err := svc.Scan(params)
+	result, err := svc.Query(params)
 	if err!= nil{
 		log.Println("error scanning")
 	}
